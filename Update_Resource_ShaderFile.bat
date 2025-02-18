748cce0171d5a1d38bcb82fe3fe6426846e43a33
Badzoker <jonghan0906@naver.com>
2025-02-18 14:43:57 +0900

    New Batch 및 Bin_Resource_Shader

---
+++ Update_Resource_ShaderFile.bat
@@ -0,0 +1,7 @@
+
+
+xcopy			/y/s			.\Resource&ShaderFile\Resource\*.*								.\Client\Bin\Resources\
+
+xcopy			/y/s			.\Resource&ShaderFile\ShaderFile\*.*							.\Client\Bin\ShaderFiles\
+
+xcopy			/y/s			.\Resource&ShaderFile\Engine_ShaderFile\*.*						.\Engine\Bin\ShaderFiles\
