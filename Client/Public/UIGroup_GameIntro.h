008949566560b49c16f055270b354105f0900ace
BHIN <vksek375@gmail.com>
2025-03-03 13:21:05 +0900

    유빈 - ui 이미지 추가 - ui 데이터 파일 추가 - 텍스트 로드 코드 주석처리

---
+++ Client/Public/UIGroup_GameIntro.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Client_Defines.h"
+#include "UIObject.h"
+
+BEGIN(Client)
+
+class CUIGroup_GameIntro final : public CUIObject
+{
+private:
+	CUIGroup_GameIntro(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CUIGroup_GameIntro(const CUIGroup_GameIntro& Prototype);
+	virtual ~CUIGroup_GameIntro() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+
+public:
+	HRESULT Ready_UIObject();
+
+private:
+	vector<UI_TextInfo> m_TextInfo = {};
+
+public:
+	static CUIGroup_GameIntro* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg) override;
+	virtual void Free() override;
+};
+
+END
