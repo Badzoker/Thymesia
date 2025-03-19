951483fa643ab5a1bc31a136bd3d09cb14bf53e5
Badzoker <jonghan0906@naver.com>
2025-03-19 10:58:59 +0900

    Circus Navi 추가

--- Client/Private/Loader.cpp
+++ Client/Private/Loader.cpp
@@ -1174,15 +1174,16 @@ HRESULT CLoader::Loading_For_Level_GamePlay()
 #pragma region Navigation 
 	lstrcpyW(m_szLoadingText, TEXT("네비게이션 원형을 생성한다."));	
 
-	// 03.13 전까지 네비
-	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Navigation"),
-	//	CNavigation::Create(m_pDevice, m_pContext, TEXT("../Bin/DataFiles/NavigationFiles/TestNavigation8.txt")))))
-	//	return E_FAIL;
-
-
-	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Navigation"), 
+	// 03.19 네비 Tutorial Map 
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Navigation"),
 		CNavigation::Create(m_pDevice, m_pContext, TEXT("../Bin/DataFiles/NavigationFiles/TestNavigation9.txt")))))
 		return E_FAIL;
+
+	// 03.19 네비 Circus Map 
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Navigation"),
+	//	CNavigation::Create(m_pDevice, m_pContext, TEXT("../Bin/DataFiles/NavigationFiles/TestNavigation10.txt")))))
+	//	return E_FAIL;
+	
 
 
 
