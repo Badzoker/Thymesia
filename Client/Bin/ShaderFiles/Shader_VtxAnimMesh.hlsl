12376cb3cb2f54cc6df9a64710da2666423e50f5
tjsghks2547 <tjsghks2547@naver.com>
2025-04-14 17:14:08 +0900

    망토 최종 수정 ( 플레이어 달라기 할때 망토는 강제로 움직임을 입혀야할거같아요..)

--- Client/Bin/ShaderFiles/Shader_VtxAnimMesh.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxAnimMesh.hlsl
@@ -689,4 +689,16 @@ technique11 DefaultTechnique
         GeometryShader = NULL;
         PixelShader = compile ps_5_0 PS_PLAYER_CLAW_DISSOLVE();
     }
+
+    pass NON_CULL_DefaultPass // 9
+    {
+
+        SetRasterizerState(Rs_Cull_NONE);
+        SetDepthStencilState(DSS_Default, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN();
+    }
 }
